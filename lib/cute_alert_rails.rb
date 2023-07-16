519cafb7336f9d83a46709090838701b803ff96f
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-16 16:41:12 +0300

    fix compiling bug

--- lib/cute_alert_rails.rb
+++ lib/cute_alert_rails.rb
@@ -3,5 +3,12 @@
 require_relative "cute_alert_rails/version"
 
 module CuteAlertRails
-  class Engine < ::Rails::Engine; end
+  class Engine < ::Rails::Engine
+    PRECOMPILE_ASSETS = Dir[root.join("app/assets/javascripts/**/*")]
+    initializer 'rails_charts.assets' do |app|
+      if app.config.respond_to?(:assets)
+        app.config.assets.precompile += PRECOMPILE_ASSETS
+      end
+    end
+  end
 end
